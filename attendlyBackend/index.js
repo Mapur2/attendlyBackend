b664fc1c92675b25096249c86ba6e20a6a17aad9
Mapur2 <rupammodak1@gmail.com>
2025-12-18 20:27:25 +0530

    cors

--- attendlyBackend/index.js
+++ attendlyBackend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "*",
     credentials: true // allow frontend to access cookies
 }));
 app.use(cookieParser());
